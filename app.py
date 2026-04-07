ad26b8a8bbdcb3231c1bf39e4892406f7365b48a
alex <[pm.it.asf@gmail.com]>
2026-04-07 22:20:02 +0300

    built and successfully launched

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+# Инициализация приложения Flask
+# # Flask автоматически ищет шаблоны в папке /templates и статику в /static
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Рендерим главную страницу лендинга
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Запуск сервера в режиме отладки
+    # host='0.0.0.0' позволяет обращаться к серверу по IP-адресу в локальной сети
+    app.run(debug=True, host='0.0.0.0', port=5001)
